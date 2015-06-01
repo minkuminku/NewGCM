7b22c3b3a3e4e3afa5a7ffb7d7ff1fa15c5cbae0
minkuminku <mayankgupta.india@gmail.com>
2015-06-01 14:54:34 +0530

    Initial commit

---
+++ app/src/main/java/com/punbook/newgcm/ApplicationConstants.java
@@ -0,0 +1,13 @@
+package com.punbook.newgcm;
+
+public interface ApplicationConstants {
+
+	// Php Application URL to store Reg ID created
+	static final String APP_SERVER_URL = "http://www.punbook.com/test/3387/Android/gcm.php?shareRegId=true";
+
+	// Google Project Number
+	static final String GOOGLE_PROJ_ID = "773123988679";
+	// Message Key
+	static final String MSG_KEY = "m";
+
+}
